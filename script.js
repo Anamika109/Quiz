696918b601c9c382e0c782562d0c8d527b94b2ed
Anamika109 <anamika.deepak19@gmail.com>
2026-08-17 19:36:07 -0400

    Start of Quiz

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let question = "What is the smallest country in the world?" 
+let answer = prompt(question)
+let correct = "Vatican City"
+if (answer === correct) {
+    console.log("You got it correct!")
+}
+else {
+    console.log("You weren't even close")
+}
+
 // let sports = [ "basketball", "badminton", "baseball", "beach volley ball", "bicycling"];
 // // for ( let i = 0; i < sports.length; i ++) {
 // //     console.log(sports[i])
@@ -6,20 +16,50 @@
 //     console.log(sport)
 // }
 
-const student = { name:"Anamika", age:10, class:"coding"}
-console.log(student.name)
+// const student = { name:"Anamika", age:10, class:"coding"}
+// console.log(student.name)
+// const car = { brand:"Audi", colour:"blue", owner:"Dan Edward", price:"43,000" , type:"car"}
+// console.log(car.brand)
+// console.log( car.owner + " paid $" + car.price + " to buy this " + car.type)
+// console.log("The colour of this expensive " + car.type + " is the colour " + car.colour)
 
-const iceCream = { flavour:"Icing On The Cake", toppings:"Whipped cream and sprinkles", Holder:"Waflle cone", holderColour:"Aqua blue"}
-console.log("The toppings of this AMAZING ice-cream are " + iceCream.toppings)
-console.log("I got this ice-cream in a " + iceCream.Holder)
-console.log("The colour of the ice-cream " + iceCream.Holder + " is " + iceCream.holderColour)
-console.log("The flavour of this AMAZING ice-cream is " + iceCream.flavour)
-if (student.age > 16) {
-    console.log("You've been able to drive so go get a job")
-}
-else if (student.age === 16) {
-    console.log("YOU CAN DRIVE NOW!")
-}
-else {
-    console.log("Go live your childhood")
-}
+// let students = [{name:"You", 
+//                  grade:100},
+
+//                 {name:"Me",
+//                  grade:82},
+
+//                 {name:"Us",
+//                  grade:46} 
+// ];
+
+// for ( let student of students ) {
+//     console.log(student.name);
+//     if (student.grade > 80) {
+//     console.log("You're so smart") 
+// }
+//    else if ( student.grade < 80 ) {
+//     console.log("Study more")
+//    } 
+
+//    else {
+//     console.log("average")
+//    }
+// }
+
+// // console.log(`${car.owner} bought this car `)
+
+// // const iceCream = { flavour:"Icing On The Cake", toppings:"Whipped cream and sprinkles", Holder:"Waflle cone", holderColour:"Aqua blue"}
+// // console.log("The toppings of this AMAZING ice-cream are " + iceCream.toppings)
+// // console.log("I got this ice-cream in a " + iceCream.Holder)
+// // console.log("The colour of the ice-cream " + iceCream.Holder + " is " + iceCream.holderColour)
+// // console.log("The flavour of this AMAZING ice-cream is " + iceCream.flavour)
+// // if (student.age > 16) {
+// //     console.log("You've been able to drive so go get a job")
+// // }
+// // else if (student.age === 16) {
+// //     console.log("YOU CAN DRIVE NOW!")
+// }
+// else {
+//     console.log("Go live your childhood")
+// }
