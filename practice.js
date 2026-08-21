e1b451a38276433b819107d9f845c18eeabb42cf
Anamika109 <anamika.deepak19@gmail.com>
2026-08-20 20:01:02 -0400

    Quiz finish

---
+++ practice.js
@@ -0,0 +1,55 @@
+// let sports = [ "basketball", "badminton", "baseball", "beach volley ball", "bicycling"];
+// // for ( let i = 0; i < sports.length; i ++) {
+// //     console.log(sports[i])
+// // }
+// for (let sport of sports) {
+//     console.log(sport)
+// }
+
+// const student = { name:"Anamika", age:10, class:"coding"}
+// console.log(student.name)
+// const car = { brand:"Audi", colour:"blue", owner:"Dan Edward", price:"43,000" , type:"car"}
+// console.log(car.brand)
+// console.log( car.owner + " paid $" + car.price + " to buy this " + car.type)
+// console.log("The colour of this expensive " + car.type + " is the colour " + car.colour)
+
+// let students = [{name:"You", 
+//                  grade:100},
+
+//                 {name:"Me",
+//                  grade:82},
+
+//                 {name:"Us",
+//                  grade:46} 
+// ];
+
+// for ( let student of students ) {
+//     console.log(student.name);
+//     if (student.grade > 80) {
+//     console.log("You're so smart") 
+// }
+//    else if ( student.grade < 80 ) {
+//     console.log("Study more")
+//    } 
+
+//    else {
+//     console.log("average")
+//    }
+// }
+
+// // console.log(`${car.owner} bought this car `)
+
+// // const iceCream = { flavour:"Icing On The Cake", toppings:"Whipped cream and sprinkles", Holder:"Waflle cone", holderColour:"Aqua blue"}
+// // console.log("The toppings of this AMAZING ice-cream are " + iceCream.toppings)
+// // console.log("I got this ice-cream in a " + iceCream.Holder)
+// // console.log("The colour of the ice-cream " + iceCream.Holder + " is " + iceCream.holderColour)
+// // console.log("The flavour of this AMAZING ice-cream is " + iceCream.flavour)
+// // if (student.age > 16) {
+// //     console.log("You've been able to drive so go get a job")
+// // }
+// // else if (student.age === 16) {
+// //     console.log("YOU CAN DRIVE NOW!")
+// }
+// else {
+//     console.log("Go live your childhood")
+// }
--- script.js
+++ script.js
@@ -1,65 +1,43 @@
-let question = "What is the smallest country in the world?" 
-let answer = prompt(question)
-let correct = "Vatican City"
-if (answer === correct) {
-    console.log("You got it correct!")
-}
-else {
-    console.log("You weren't even close")
-}
-
-// let sports = [ "basketball", "badminton", "baseball", "beach volley ball", "bicycling"];
-// // for ( let i = 0; i < sports.length; i ++) {
-// //     console.log(sports[i])
-// // }
-// for (let sport of sports) {
-//     console.log(sport)
-// }
-
-// const student = { name:"Anamika", age:10, class:"coding"}
-// console.log(student.name)
-// const car = { brand:"Audi", colour:"blue", owner:"Dan Edward", price:"43,000" , type:"car"}
-// console.log(car.brand)
-// console.log( car.owner + " paid $" + car.price + " to buy this " + car.type)
-// console.log("The colour of this expensive " + car.type + " is the colour " + car.colour)
-
-// let students = [{name:"You", 
-//                  grade:100},
-
-//                 {name:"Me",
-//                  grade:82},
-
-//                 {name:"Us",
-//                  grade:46} 
-// ];
-
-// for ( let student of students ) {
-//     console.log(student.name);
-//     if (student.grade > 80) {
-//     console.log("You're so smart") 
-// }
-//    else if ( student.grade < 80 ) {
-//     console.log("Study more")
-//    } 
-
-//    else {
-//     console.log("average")
-//    }
-// }
-
-// // console.log(`${car.owner} bought this car `)
-
-// // const iceCream = { flavour:"Icing On The Cake", toppings:"Whipped cream and sprinkles", Holder:"Waflle cone", holderColour:"Aqua blue"}
-// // console.log("The toppings of this AMAZING ice-cream are " + iceCream.toppings)
-// // console.log("I got this ice-cream in a " + iceCream.Holder)
-// // console.log("The colour of the ice-cream " + iceCream.Holder + " is " + iceCream.holderColour)
-// // console.log("The flavour of this AMAZING ice-cream is " + iceCream.flavour)
-// // if (student.age > 16) {
-// //     console.log("You've been able to drive so go get a job")
-// // }
-// // else if (student.age === 16) {
-// //     console.log("YOU CAN DRIVE NOW!")
+// let question = "What is the smallest country in the world?" 
+// let answer = prompt(question)
+// let correct = "Vatican City"
+// if (answer === correct) {
+//     console.log("You got it correct!")
 // }
 // else {
-//     console.log("Go live your childhood")
+//     console.log("You were wrong!")
+// }
+
+// const quiz = { question: "What is the smallest country in the world?" , answer: "Vatican City"}
+// const quiz2 = { question: "What is the biggest country in the world?" , answer: "Russia"}
+let questions = [{ question: "What is the smallest country in the world?" ,     answer: "Vatican City"}  
+,
+                { question: "What is the biggest country in the world?" , answer: "Russia"} ,
+                { question: "Which country is known as the Land of the Rising Sun?" , answer: "Japan"} , 
+                { question: "Which country is home to the Great Barrier Reef?" , answer: "Australia"} ,
+                { question: "What country has the largest population in the world?" , answer: "India"} ]
+
+let counter = 0
+
+for ( let q of questions ) {
+    // console.log(q.question);
+    let answer = prompt(q.question)
+    if (answer === q.answer) {
+        alert("You got it correct!")
+        counter ++
+    }
+    else {
+        alert("You were wrong!")
+    }
+}
+// let filler = counter + "/" + questions.length
+document.getElementById("score").innerHTML= counter + "/" + questions.length
+
+// let fruits = [{name: "apple", colour: "red"}, {name: "blueberry", colour:"blue"}]
+
+// for (let fruit of fruits) {
+//     let ans = prompt("whats the colour of" + fruit.name)
+//     if (ans === fruit.colour) {
+//         alert("wow")
+//     }
 // }
